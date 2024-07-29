7239f06454df6d398f8b6b0dc8a229122d7efa8f
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-07-29 22:45:42 +0200

    improve gameplay

--- main.py
+++ main.py
@@ -20,14 +20,42 @@ def show_cards(user_hand: list):
     for item in user_hand:
         print(f"{item.Name} of {item.Suit}")
 
-user_hand = []
-user_hand.append(decks.pop(0))
-
-
-
-needMore: bool = True
-while needMore:
-    user_hand.append(decks.pop(0))
-    clear()
+def show_user_hand(user_hand: list):
     show_cards(user_hand)
     print(f"Your hand has value of '{hand.get_value(user_hand)}")
+
+def make_user_hand():
+    user_hand = []
+    user_hand.append(decks.pop(0))
+    user_hand.append(decks.pop(0))
+    needMore: bool = True
+    value: int = 0
+
+    show_user_hand(user_hand)
+
+    while needMore and value < 21:
+        need_more_str = input("Need more?")
+        needMore = need_more_str.lower() == "y" or need_more_str.lower() == "yes"
+        if needMore:
+            user_hand.append(decks.pop(0))
+            value = hand.get_value(user_hand)
+            show_user_hand(user_hand)
+
+    return value
+
+
+continue_game: str = "y"
+games_passed: int = 0
+
+while continue_game == "y" or continue_game == "yes":
+    if games_passed > 0:
+        clear()
+        print(f"Game No: {games_passed + 1}")
+
+    user_value = make_user_hand()
+    games_passed += 1
+    if user_value > 21:
+        print("Bust!")
+    
+    continue_game = input("One more game? ")
+    
