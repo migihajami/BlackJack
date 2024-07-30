b11e36de79bf89fc718f72533d84b339d2d80206
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-07-30 17:02:18 +0200

    Implement game against dealer

--- hand.py
+++ hand.py
@@ -1,4 +1,5 @@
 import card
+import time
 
 def get_value(cards: list):
     result: int = 0
@@ -15,25 +16,33 @@ def get_value(cards: list):
             result += item.Value
     return result
 
-def show_cards(user_hand: list):
+def show_cards(user_hand: list, is_dealer: bool = False):
     item: card.Card
 
-    print("Your cards:")
+    if is_dealer:
+        print("Dealer has: ")
+    else:
+        print("Your cards:")
+
     for item in user_hand:
         print(f"{item.Name} of {item.Suit}")
 
-def show_user_hand(user_hand: list):
-    show_cards(user_hand)
-    print(f"Your hand has value of '{get_value(user_hand)}")
+def show_hand(hand: list, is_dealer: bool = False):
+    show_cards(hand, is_dealer)
+    if is_dealer:
+        print(f"Dealer has {get_value(hand)}")
+    else:
+        print(f"Your hand has value of '{get_value(hand)}")
 
-def make_user_hand(decks: list):
+def make_user_hand(decks: list, card1: card.Card, card2: card.Card):
     user_hand = []
-    user_hand.append(decks.pop(0))
-    user_hand.append(decks.pop(0))
+    user_hand.append(card1)
+    user_hand.append(card2)
     needMore: bool = True
     value: int = get_value(user_hand)
 
-    show_user_hand(user_hand)
+    print("========================================")
+    show_hand(user_hand)
 
     while needMore and value < 21:
         need_more_str = input("Need more?")
@@ -41,6 +50,26 @@ def make_user_hand(decks: list):
         if needMore:
             user_hand.append(decks.pop(0))
             value = get_value(user_hand)
-            show_user_hand(user_hand)
+            print("------")
+            show_hand(user_hand)
 
     return value
+
+def make_dealer_hand(decks: list, first_card: card.Card):
+    dealer_hand = []
+    dealer_hand.append(first_card)
+    dealer_hand.append(decks.pop(0))
+    
+    value: int = get_value(dealer_hand)
+    print("========================================")
+    show_hand(dealer_hand, True)
+    while value < 17:
+        time.sleep(1.5)
+        dealer_hand.append(decks.pop(0))
+        value = get_value(dealer_hand)
+        show_hand(dealer_hand, True)
+        print("------")
+    
+    return value
+        
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import art
 import shuffle
 import hand
+from card import Card
 from replit import clear 
 
 
@@ -20,11 +21,25 @@ while continue_game == "y" or continue_game == "yes":
         print(f"There are {len(decks)} cards left in the deck")
         print(f"Game No: {games_passed + 1}")
 
-    user_value = hand.make_user_hand(decks)
+    user_card1 = decks.pop(0)
+    dealer_card1: Card = decks.pop(0)
+    user_card2 = decks.pop(0)
+
+    print(f"Dealer has {dealer_card1.Name} of {dealer_card1.Suit}")
+
+    user_value = hand.make_user_hand(decks, user_card1, user_card2)
     games_passed += 1
     if user_value > 21:
         print("Bust!")
-    
+    else:
+        dealer_value = hand.make_dealer_hand(decks, dealer_card1)
+        if dealer_value < user_value or dealer_value > 21:
+            print("You win1")
+        elif dealer_value == user_value:
+            print("Push.")
+        else:
+            print("You loose.")
+
     continue_game = input("One more game? ")
     if len(decks) < 5:
         decks = shuffle.stir(1)
