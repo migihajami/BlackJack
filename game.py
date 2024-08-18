31635f4a019135bf00abba1bf3fbdc00122ea050
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-08-18 20:01:02 +0200

    move gameplay into the class Game

---
+++ game.py
@@ -0,0 +1,70 @@
+from HandDisplayBase import HandDisplay
+from dealer import Dealer
+from player import Player
+from shuffle import Shuffle
+from stattistics import Statistics
+from replit import clear
+
+
+class Game:
+    stats: Statistics
+
+    def __init__(self, player_name, hand_displayer: HandDisplay, number_of_decks: int = 2):
+        self.shuffle = Shuffle(number_of_decks)
+        self.rounds_passed = 0
+        self.score = {"dealer": 0, "player": 0}
+        self.stats = Statistics()
+        self.hand_displayer = hand_displayer
+        self.player = Player(player_name, self.shuffle, self.hand_displayer, 1)
+        self.dealer = Dealer(self.shuffle, self.hand_displayer, 2)
+        self.continue_game = "y"
+
+    def get_stats(self) -> Statistics:
+        return self.stats
+
+    def run(self):
+        while self.continue_game == "y" or self.continue_game == "yes":
+            if self.rounds_passed > 0:
+                clear()
+                print(f"There are {len(self.shuffle)} cards left in the deck")
+                print(f"Game No: {self.rounds_passed + 1}")
+                print(self.stats)
+                print("========================================")
+
+            self.player.hit(0)
+            self.dealer.hit()
+            self.player.hit(0)
+            self.dealer.hit()
+
+            self.hand_displayer.display_card(self.dealer.name, self.dealer.get_first_card())
+            print("----------------------------------------")
+
+            if self.dealer.has_blackjack() and not self.player.has_blackjack(0):
+                user_value = self.player.get_value(0)
+                self.hand_displayer.display_hand(self.player.name, self.player.hands[0])
+            else:
+                user_value = self.player.make_hand(0)
+
+            self.rounds_passed += 1
+            if user_value > 21:
+                print("Bust!")
+            else:
+                if not self.player.has_blackjack(0):
+                    dealer_value = self.dealer.make_hand()
+                else:
+                    dealer_value = self.dealer.get_value()
+                    self.hand_displayer.display_hand(self.dealer.name, self.dealer.hands[0])
+
+                if dealer_value < user_value or dealer_value > 21:
+                    print("You win!")
+                    self.stats.win()
+                elif dealer_value == user_value:
+                    print("Push.")
+                    self.stats.push()
+                else:
+                    print("You loose.")
+                    self.stats.loose()
+
+            self.dealer.flush()
+            self.player.flush()
+            self.continue_game = input("One more game? ")
--- main.py
+++ main.py
@@ -1,73 +1,24 @@
 import art
+from game import Game
 from HandDisplayBase import ConsoleHandDisplay
-from dealer import Dealer
-from player import Player
-from shuffle import Shuffle
-from replit import clear
-from stattistics import  Statistics
-
-shuffle = Shuffle(2)
-
-continue_game: str = "y"
-games_passed: int = 0
-score = {"dealer": 0, "player": 0}
 
 print(art.logo)
 print("Welcome to the table! Black Jack 21. Dealer must hit on 16 and stop on 17.")
 
-statistics = Statistics()
-print(statistics)
 hand_displayer = ConsoleHandDisplay()
-
 name = input("What is your name?")
-player = Player(name, shuffle,  hand_displayer, 1)
-dealer = Dealer(shuffle, hand_displayer, 2)
+game = Game(name, hand_displayer, 2)
+print(game.stats)
+
+game.run()
+print("Bye!")
+print(game.stats)
 
 
-while continue_game == "y" or continue_game == "yes":
-    if games_passed > 0:
-        clear()
-        print(f"There are {len(shuffle)} cards left in the deck")
-        print(f"Game No: {games_passed + 1}")
-        print(statistics)
-        print("========================================")
 
-    player.hit(0)
-    dealer.hit()
-    player.hit(0)
-    dealer.hit()
 
-    hand_displayer.display_card(dealer.name, dealer.get_first_card())
-    print("----------------------------------------")
 
-    if dealer.has_blackjack() and not player.has_blackjack(0):
-        user_value = player.get_value(0)
-        hand_displayer.display_hand(player.name, player.hands[0])
-    else:
-        user_value = player.make_hand(0)
 
-    games_passed += 1
-    if user_value > 21:
-        print("Bust!")
-    else:
-        if not player.has_blackjack(0):
-            dealer_value = dealer.make_hand()
-        else:
-            dealer_value = dealer.get_value()
-            hand_displayer.display_hand(dealer.name, dealer.hands[0])
 
-        if dealer_value < user_value or dealer_value > 21:
-            print("You win!")
-            statistics.win()
-        elif dealer_value == user_value:
-            print("Push.")
-            statistics.push()
-        else:
-            print("You loose.")
-            statistics.loose()
-
-    dealer.flush()
-    player.flush()
-    continue_game = input("One more game? ")
 
     
