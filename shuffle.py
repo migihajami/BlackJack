dc2750f57389d310ebbc326c68b3451ee9539d54
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-08-18 14:35:46 +0200

    make suits and card_set private and static

--- shuffle.py
+++ shuffle.py
@@ -1,29 +1,29 @@
 import random
 import card
 
-CARD_SET: dict = {
-    'Two': {"Nick": "2", "Value": 2},
-    'Three': {"Nick": "3", "Value": 3},
-    'Four': {"Nick": "4", "Value": 4},
-    'Five': {"Nick": "5", "Value": 5},
-    'Six': {"Nick": "6", "Value": 6},
-    'Seven': {"Nick": "7", "Value": 7},
-    'Eight': {"Nick": "8", "Value": 8},
-    'Nine': {"Nick": "9", "Value": 9},
-    'Ten': {"Nick": "10", "Value": 10},
-    'Jack': {"Nick": "J", "Value": 10},
-    'Queen': {"Nick": "Q", "Value": 10},
-    'King': {"Nick": "K", "Value": 10},
-    'Ace': {"Nick": "A", "Value": 11}
-}
-
-SUITS = ["Hearts", "Clubs", "Diamonds", "Spades"]
-
 
 class Shuffle:
     sorted_deck: list
     dec_amount: int
     deck: list
+
+    _card_set: dict = {
+        'Two': {"Nick": "2", "Value": 2},
+        'Three': {"Nick": "3", "Value": 3},
+        'Four': {"Nick": "4", "Value": 4},
+        'Five': {"Nick": "5", "Value": 5},
+        'Six': {"Nick": "6", "Value": 6},
+        'Seven': {"Nick": "7", "Value": 7},
+        'Eight': {"Nick": "8", "Value": 8},
+        'Nine': {"Nick": "9", "Value": 9},
+        'Ten': {"Nick": "10", "Value": 10},
+        'Jack': {"Nick": "J", "Value": 10},
+        'Queen': {"Nick": "Q", "Value": 10},
+        'King': {"Nick": "K", "Value": 10},
+        'Ace': {"Nick": "A", "Value": 11}
+    }
+
+    _suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
 
     def __init__(self, dec_amount: int):
         '''
@@ -38,8 +38,8 @@ class Shuffle:
     def shuffle_init(self):
         self.sorted_deck.clear()
         for _ in range(self.dec_amount):
-            for suit in SUITS:
-                block = [card.Card(item, suit, value["Value"], value["Nick"]) for (item, value) in CARD_SET.items()]
+            for suit in self._suits:
+                block = [card.Card(item, suit, value["Value"], value["Nick"]) for (item, value) in self._card_set.items()]
                 self.sorted_deck.extend(block)
 
     def stir(self):
