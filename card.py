3f3e25f8d5bfa12a4e33c47a2f0eda72c2885c0e
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-08-18 14:28:37 +0200

    make suit_symbols private

--- card.py
+++ card.py
@@ -7,14 +7,14 @@ class Card:
     nick: str
     suit_symbol: str
 
-    suit_symbols = {"Hearts": "\u2665", "Clubs": "\u2663", "Diamonds": "\u2666", "Spades": "\u2660"}
+    _suit_symbols = {"Hearts": "\u2665", "Clubs": "\u2663", "Diamonds": "\u2666", "Spades": "\u2660"}
 
     def __init__(self, name, suit, value, nick):
         self.name = name
         self.suit = suit
         self.value = value
         self.nick = nick
-        self.suit_symbol = self.suit_symbols[suit]
+        self.suit_symbol = self._suit_symbols[suit]
 
     def __str__(self):
         return f"[{self.nick}{self.suit_symbol}]"
