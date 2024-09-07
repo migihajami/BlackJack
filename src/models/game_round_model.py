2f877ee1343dd3b583e6774d70fd75ae7e4bd59c
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-09-07 22:57:47 +0200

    Add round result

--- src/models/game_round_model.py
+++ src/models/game_round_model.py
@@ -1,7 +1,15 @@
+from enum import Enum
 from typing import List
 from pydantic import BaseModel
 from src.models.card_model import CardModel
 from src.models.game_model import GameStateEnum
+
+
+class RoundResultEnum(Enum):
+    NOT_FINISHED = 1,
+    PLAYER_WIN = 2,
+    DEALER_WIN = 3,
+    PUSH = 4,
 
 
 class GameRoundModel(BaseModel):
@@ -11,3 +19,4 @@ class GameRoundModel(BaseModel):
     dealer_value: int
     round_state: GameStateEnum
     bet_amount: float
+    round_result: RoundResultEnum
